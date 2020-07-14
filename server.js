75cdde31bb24a65e0bea53d18a47a155552566f6
longshot099 <jaqiu123@gmail.com>
2020-07-13 20:34:05 -0700

    changed

---
+++ server.js
@@ -0,0 +1,47 @@
+// ----------------------back end -------------------------
+const express = require('express');
+const socket = require('socket.io');
+const mongo = require('mongodb').MongoClient;
+
+let app = express();
+
+app.use(express.static('static')); // allows all the html/css and js files to be connected to the back end server
+
+let server = app.listen(3000);
+let backSocket = socket(server);
+
+mongo.connect('mongodb://127.0.0.1:27017/chat',function(err,db){
+  let chat = db.collection('chats'); //chat is an object of messages
+  
+  if(err) throw err;
+  
+  else{
+    backSocket.on('connection',function(socket){
+      chat.find().limit(100).sort({_id:1}).toArray(function(err,result){
+        if(err) throw err;
+        
+        else{
+          socket.emit('output',result); // automattically load messages for corresponding user
+        }
+      })
+      socket.on('message',function(data){
+        chat.insert(
+          {
+            name:data.name, 
+            message:data.message
+          },function(){
+            backSocket.emit('output',[data]); // [data] = [{1 name, 1 message}]
+          })
+        
+      });
+
+      socket.on('clear',function(){
+        chat.remove({});
+      });
+      socket.on('typing',function(data){
+       socket.broadcast.emit('typing',data)
+      })
+     });
+  }
+})// end of mongo
+
---
+++ static/index.js
@@ -0,0 +1,66 @@
+// -------------------front end---------------------------
+let frontSocket = io.connect('http://localhost:3000');
+
+
+let nameRef  = document.getElementById('name')
+let messageRef = document.getElementById('message')
+let output = document.getElementById('output');
+let feedback = document.getElementById('feedback');
+let sendBtnRef = document.getElementById('send-btn');
+let clear = document.getElementById('clear');
+
+sendBtnRef.addEventListener('click',function(){
+    if(!messageRef.value || !nameRef.value){
+     return;
+    }
+    frontSocket.emit('message',{
+      name:nameRef.value,
+      message:messageRef.value
+    });
+});
+
+clear.addEventListener('click',function(){ // clear
+  frontSocket.emit('clear');
+  while (output) // as long as there is a message, each one will be removed.
+      output.removeChild(output.firstChild);
+
+  
+});
+document.addEventListener('keydown',function(e){
+  if(e.key == 'Enter'){
+    if(!messageRef.value || !nameRef.value){
+      return;
+     }
+     frontSocket.emit('message',{
+       name:nameRef.value,
+       message:messageRef.value
+     });
+  }
+})
+messageRef.addEventListener('input',function(){
+  if(messageRef.value != ''){
+    frontSocket.emit('typing',nameRef.value)
+  }
+  else{
+    frontSocket.emit('typing', '');
+  }
+})
+frontSocket.on('output',function(data){ 
+  if(data.length){ // if there are messages stored in the data base, 
+                  //it'll execute this loop to print out all the messages
+    for(let i = 0; i < data.length; i++){
+      let message = document.createElement('div');
+      message.textContent = data[i].name + ':' + data[i].message;
+      output.append(message);
+    }
+  }
+  messageRef.value = '';
+  feedback.innerHTML = ''
+});
+frontSocket.on('typing',function(data){
+  if(data){
+    feedback.innerHTML = '<p><em>' + data + ' is typing... </em></p>';
+  }
+  else feedback.innerHTML = '';
+})
+
